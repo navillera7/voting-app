462ecb6a99e8725f6488c6c4442496fdb82e18fc
navillera7 <102697353+navillera7@users.noreply.github.com>
2025-04-25 09:05:08 -0400

    Update UserCode.js

--- models/UserCode.js
+++ models/UserCode.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose');
 const userCodeSchema = new mongoose.Schema({
   code: { type: String, unique: true },
   voted: { type: Boolean, default: false },
-  votes: {
-    type: Map,
-    of: Number,
-    default: {}
-  }
+votes: { type: String } // 또는 'vote' 단수로 변경해도 됨
+
 });
 
 module.exports = mongoose.models.UserCode || mongoose.model('UserCode', userCodeSchema);
